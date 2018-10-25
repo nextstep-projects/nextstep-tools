023801b48a1852ba7d437086f4294215c457fba7
javajigi <javajigi@gmail.com>
2018-10-25 15:17:52 +0900

    initial commit

---
+++ shells/checkout_pr.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+current_dir=$(pwd)
+echo "current dir : $current_dir"
+
+workspace=~/codesquad/review-workspace
+
+reg="https://github.com/code-squad/(.*)/pull/(.*)"
+
+function clone_repository {
+  cd $workspace
+
+  directory=$1-$2
+  if [ ! -d "$directory" ]; then
+    echo "clone $directory repository"
+    git clone https://github.com/code-squad/$1.git $directory
+  fi
+}
+
+# function checkout_branch {
+#   cd $workspace/$1-$2
+#   echo "current dir : $(pwd)"
+
+#   branch_name=$(git branch -l | grep $1)
+#   echo "branch name : $branch_name"
+#   if [ ! "$branch_name" ]; then
+#     git checkout -t origin/$1
+#   fi
+# }
+
+function fetch_reset {
+  git fetch origin pull/$1/head:pull_$1
+  git checkout pull_$1
+  git branch -a
+}
+
+if [ ! "$1" ]; then
+  echo "checkout할 pull request 경로를 입력하세요"
+  exit
+fi
+
+if [[ $1 =~ $reg ]]
+then
+  echo "id ${BASH_REMATCH[1]}, name ${BASH_REMATCH[2]}"
+  clone_repository ${BASH_REMATCH[1]} ${BASH_REMATCH[2]}
+  cd $workspace/${BASH_REMATCH[1]}-${BASH_REMATCH[2]}
+  fetch_reset ${BASH_REMATCH[2]}
+else
+  echo "does'not match"
+fi
---
+++ shells/create-branch.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+current_dir=$(pwd)
+echo "current dir : $current_dir"
+
+workspace=~/codesquad/workspace
+
+function clone_repository {
+  cd $workspace
+
+  if [ ! -d "$1" ]; then
+    git clone https://github.com/code-squad/$1.git
+  fi
+}
+
+function sync_branch {
+  echo "sync $1 from remote to local"
+  cd $workspace/$1
+  git fetch --prune
+}
+
+function create_branch {
+  branch_name=$(git branch -r | grep origin/$1)
+  echo "branch name : $branch_name"
+  if [ ! "$branch_name" ]; then
+    echo "create $1 local branch"
+    git checkout -b $1 origin/master
+
+    echo "push $1 branch"
+    git push --set-upstream origin $1
+  fi
+}
+
+function create_branches {
+  while read github_id; do    
+      create_branch $github_id   
+  done < $current_dir/$1.txt
+
+  git checkout master
+}
+
+while read project_id; do
+  clone_repository $project_id
+  sync_branch $project_id
+  create_branches $project_id
+done < $current_dir/projects.txt
